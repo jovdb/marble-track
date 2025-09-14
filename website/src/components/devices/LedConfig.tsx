c15262c00804f41f85126f66fa43974653ebcc57
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2025-09-14 23:08:43 +0200

    Add Name to ledConfig

--- website/src/components/devices/LedConfig.tsx
+++ website/src/components/devices/LedConfig.tsx
@@ -1,25 +1,47 @@
 import { createSignal } from "solid-js";
 import DeviceConfig from "./DeviceConfig";
+import { setupMotor } from "../../stores/PwmMotor";
 
 interface LedConfigProps {
   id: string;
 }
 
 export default function LedConfig(props: LedConfigProps) {
-  const [pin, setPin] = createSignal(1);
+  const [name, setName] = createSignal("Led");
+  const [pin, setPin] = createSignal(props);
 
   return (
-    <DeviceConfig id={props.id} onSave={() => alert(`TODO: save`)}>
-      <label>
-        Pin number:
-        <input
-          type="number"
-          value={pin()}
-          min={0}
-          onInput={(e) => setPin(Number(e.currentTarget.value))}
-          style={{ "margin-left": "0.5rem" }}
-        />
-      </label>
+    <DeviceConfig
+      id={props.id}
+      onSave={() => {
+        alert("TODO: Implement LED setup");
+      }}
+    >
+      <>
+        <div>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={name()}
+              onInput={(e) => setName(e.currentTarget.value)}
+              style={{ "margin-left": "0.5rem" }}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Pin number:
+            <input
+              type="number"
+              value={pin()}
+              min={0}
+              onInput={(e) => setPin(Number(e.currentTarget.value))}
+              style={{ "margin-left": "0.5rem" }}
+            />
+          </label>
+        </div>
+      </>
     </DeviceConfig>
   );
 }

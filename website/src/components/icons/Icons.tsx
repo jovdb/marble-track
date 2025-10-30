29d322cc4d6c56685bdc4c626571841b5239c856
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2025-10-30 22:47:21 +0100

    fix icon

--- website/src/components/icons/Icons.tsx
+++ website/src/components/icons/Icons.tsx
@@ -100,15 +100,16 @@ export const RadioIcon = (props: IconProps) => (
 export const BroadcastIcon = (props: IconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
+    width={props.width || 24}
+    height={props.height || 24}
+    class={props.class}
+    style={props.style}
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
-    class="icon icon-tabler icons-tabler-outline icon-tabler-building-broadcast-tower"
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />

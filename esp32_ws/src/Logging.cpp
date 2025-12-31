6111b9e70bd9c09f3545dca3f1d12073c7beb539
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2025-12-31 16:11:34 +0100

    Improve lift

--- esp32_ws/src/Logging.cpp
+++ esp32_ws/src/Logging.cpp
@@ -7,5 +7,5 @@
 
 #if MARBLE_LOG_ENABLED
 // Initialize with all log types enabled by default
-uint8_t LogConfig::enabledTypes = LOG_INFO | LOG_WARN | LOG_ERROR; // | LOG_WS_RECEIVE | LOG_WS_SEND;
+uint8_t LogConfig::enabledTypes = LOG_DEBUG | LOG_INFO | LOG_WARN | LOG_ERROR; // | LOG_WS_RECEIVE | LOG_WS_SEND;
 #endif

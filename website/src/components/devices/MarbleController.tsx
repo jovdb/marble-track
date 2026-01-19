9d346d2cc2fae75e2cfc3f960470e9a9f24723a0
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2026-01-20 00:11:14 +0100

    Don't show empty state for marble-controller

--- website/src/components/devices/MarbleController.tsx
+++ website/src/components/devices/MarbleController.tsx
@@ -3,7 +3,7 @@ import { getDeviceIcon } from "../icons/Icons";
 
 export function MarbleController(props: { id: string }) {
   return (
-    <Device id={props.id} icon={getDeviceIcon("marblecontroller")}>
+    <Device id={props.id} icon={getDeviceIcon("marblecontroller")} stateComponent={() => null}>
       <div style={{ padding: "1rem", "text-align": "center" }}>
         <p>Marble Controller - Manages lift, buzzer, and wheel operations</p>
       </div>

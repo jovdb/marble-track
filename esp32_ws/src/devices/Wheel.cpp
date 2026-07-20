90bf7e9156349306b34dd9dd526666080954fd47
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2026-07-20 21:44:48 +0200

    remove

--- esp32_ws/src/devices/Wheel.cpp
+++ esp32_ws/src/devices/Wheel.cpp
@@ -150,6 +150,13 @@ namespace devices
                 notifyStateChanged();
             }
 
+            if (millis() % 100 == 0)
+            {
+                MLOG_DEBUG("%s: Moving... current position: %ld, target: %ld, speed: %.1f, accel: %.1f",
+                           toString().c_str(), stepperState.currentPosition, stepperState.targetPosition,
+                           stepperState.speed, stepperState.acceleration);
+            }
+
             // Check zero sensor for position tracking
             if (_zeroSensor->onPressed())
             {
@@ -436,7 +443,7 @@ namespace devices
             return false;
         }
 
-        int nextIndex = (_state.currentBreakpointIndex + 1) % _config.breakPoints.size();
+        int nextIndex = (_state.currentBreakpointIndex + 1) % _config.breakPoints.size(); // Rest on overflow
         _state.targetBreakpointIndex = nextIndex;
 
         // Calculate target angle
